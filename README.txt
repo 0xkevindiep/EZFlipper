c119d83e3fcb3f4b39a6e9b872b905656bed3402
Kevin Diep <kevindiep@utexas.edu>
2020-12-01 03:22:30 -0600

    sort items by new

--- README.txt
+++ README.txt
@@ -3,7 +3,7 @@ marketplace, Grailed, and compares the buying price to the average sale price
 within the last week on another reselling marketplace, StockX, to calculate 
 the possible profit margins from flipping that item. The user gets to choose 
 the minimum profit threshold by adjusting the variable 'min_profit' to 
-determine which items are worth reselling. 
+determine which items are worth reselling.
 
 REQUIREMENTS
 You must have Google Chrome Version 87.0.4280.67
--- kashmachine.py
+++ kashmachine.py
@@ -1,28 +1,52 @@
 import selenium
 from selenium import webdriver
 import os
+import time
 
 brands = ["supreme"]
 
 # ADJUST MINIMUM PROFIT THRESHOLD
 min_profit = 20.00
 
+# ADJUST HOW NEW ITEMS ARE IN HOURS
+max_time = 24
+
 def main(): 
 	driver_path = os.getcwd() + "/chromedriver"
 	grailed_driver = webdriver.Chrome(executable_path=driver_path)
-	stockx_driver = webdriver.Chrome(executable_path=driver_path)
+	# stockx_driver = webdriver.Chrome(executable_path=driver_path)
 	grailed_url = "https://www.grailed.com/designers/"
 	stockx_url = "https://stockx.com/"
-	stockx_driver.get(stockx_url)
+	# stockx_driver.get(stockx_url)
 
 	for i in range(len(brands)): 
 		grailed_url = grailed_url + brands[i]
 		grailed_driver.get(grailed_url)
+
+		# filter items to be brand new
 		filters = grailed_driver.find_elements_by_class_name('instant-search-collapsible._collapsed')
 		condition = filters[2]
 		condition.click()
 		new_box = grailed_driver.find_element_by_name('New/Never Worn')
 		new_box.click()
+
+		# sort items by new
+		sort_select = grailed_driver.find_element_by_class_name('ais-SortBy-select')
+		sort_select.click()
+		sorts = grailed_driver.find_elements_by_class_name('ais-SortBy-option')
+		new_sort = sorts[4]
+		new_sort.click()
+
+		
+
+		
+		
+
+
+		
+
+
+		
 
 
 
