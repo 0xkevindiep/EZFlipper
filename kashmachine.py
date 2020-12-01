a40f4f0b73402fdfa4f328221b2326c429a666b0
Kevin Diep <kevindiep@utexas.edu>
2020-12-01 00:08:53 -0600

    created separate drivers for stockx and grailed

--- kashmachine.py
+++ kashmachine.py
@@ -6,17 +6,22 @@ brands = ["supreme"]
 
 def main(): 
 	driver_path = os.getcwd() + "/chromedriver"
-	driver = webdriver.Chrome(executable_path=driver_path)
+	grailed_driver = webdriver.Chrome(executable_path=driver_path)
+	stockx_driver = webdriver.Chrome(executable_path=driver_path)
 	grailed_url = "https://www.grailed.com/designers/"
+	stockx_url = "https://stockx.com/"
+	stockx_driver.get(stockx_url)
+
 	for i in range(len(brands)): 
-		url = grailed_url + brands[i]
-		driver.get(url)
-		filters = driver.find_elements_by_class_name('instant-search-collapsible._collapsed')
+		grailed_url = grailed_url + brands[i]
+		grailed_driver.get(grailed_url)
+		filters = grailed_driver.find_elements_by_class_name('instant-search-collapsible._collapsed')
 		condition = filters[2]
 		condition.click()
-		new_box = driver.find_element_by_name('New/Never Worn')
+		new_box = grailed_driver.find_element_by_name('New/Never Worn')
 		new_box.click()
 		
+
 
 
 main()
