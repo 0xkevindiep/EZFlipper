c19f6b702b464c8abc9cc85a55c9c1b063e539da
Kevin Diep <kevindiep@utexas.edu>
2020-11-30 23:16:49 -0600

    readme

---
+++ README.txt
@@ -0,0 +1,1 @@
+Go to system preferences and allow chromedriver
--- kashmachine.py
+++ kashmachine.py
@@ -1,2 +1,11 @@
 import selenium
 from selenium import webdriver
+import os
+
+def main(): 
+	
+	driver_path = os.getcwd() + "/chromedriver"
+	driver = webdriver.Chrome(executable_path=driver_path)
+	driver.get('https://www.grailed.com/')
+
+main()
