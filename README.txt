452f9b096aacc9d923dd84d6a63c0ce79a4b6839
Kevin Diep <kevindiep@utexas.edu>
2020-11-30 23:58:36 -0600

    filters by condition

--- README.txt
+++ README.txt
@@ -1,1 +1,2 @@
 Go to system preferences and allow chromedriver
+You must have Google Chrome Version 87.0.4280.67
--- kashmachine.py
+++ kashmachine.py
@@ -2,10 +2,21 @@ import selenium
 from selenium import webdriver
 import os
 
+brands = ["supreme"]
+
 def main(): 
-	
 	driver_path = os.getcwd() + "/chromedriver"
 	driver = webdriver.Chrome(executable_path=driver_path)
-	driver.get('https://www.grailed.com/')
+	grailed_url = "https://www.grailed.com/designers/"
+	for i in range(len(brands)): 
+		url = grailed_url + brands[i]
+		driver.get(url)
+		filters = driver.find_elements_by_class_name('instant-search-collapsible._collapsed')
+		condition = filters[2]
+		condition.click()
+		new_box = driver.find_element_by_name('New/Never Worn')
+		new_box.click()
+		
+
 
 main()
