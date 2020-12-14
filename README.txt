d1f3cb89caf307a7be0e07932c52e4d581e7ac1b
Kevin Diep <kevindiep@utexas.edu>
2020-12-14 15:07:27 -0600

    authentication request

--- README.txt
+++ README.txt
@@ -1,12 +1,13 @@
-Kash Machine is a program that finds brand new items on a third party 
-marketplace, Grailed, and compares the buying price to the average sale price 
-of that same item on another reselling marketplace, StockX, to calculate 
-the possible profit margins from flipping that item. The user gets to choose 
-the minimum profit threshold by adjusting the variable 'min_profit' to 
-determine which items are worth reselling.
+Kash Machine is a program that scrapes a third party marketplace, Grailed, to 
+find brand new items and compares the buying price to the average sale price 
+of that same item on another reselling marketplace, StockX, to calculate the 
+possible profit margins from flipping that item. The user gets to choose the 
+minimum profit threshold by adjusting the variable 'min_profit' to determine 
+which items are worth reselling. With millions of listings on Grailed, there's 
+an abundance of hidden profitable items ready to be found and flipped. 
 
 REQUIREMENTS
-You must have Google Chrome Version 87.0.4280.67
+You must have the latest verion of Google Chrome (Version 87.0.4280.67)
 
 MAC OS
 When running for the first time, you may need to go to system preferences to 
--- kashmachine.py
+++ kashmachine.py
@@ -1,15 +1,18 @@
 import selenium
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.action_chains import ActionChains  
 import os
 import time
 
-brands = ["supreme"]
+brands = ["vlone"]
 
 # ADJUST MINIMUM PROFIT THRESHOLD
 min_profit = 20.00
 
 # ADJUST MAX AGE OF ITEMS IN DAYS
-max_age = 30.00
+max_age = 1.00
+
 
 class GrailedItem: 
 	def __init__(self, link, brand, name, color, size, price): 
@@ -40,40 +43,49 @@ class GrailedItem:
 
 def main(): 
 	driver_path = os.getcwd() + "/chromedriver"
-	grailed_driver = webdriver.Chrome(executable_path=driver_path)
-	# stockx_driver = webdriver.Chrome(executable_path=driver_path)
+	driver = webdriver.Chrome(executable_path=driver_path)
+
 	grailed_url = "https://www.grailed.com/designers/"
 	stockx_url = "https://stockx.com/"
-	# stockx_driver.get(stockx_url)
 
+	
 	for i in range(len(brands)): 
 		grailed_url = grailed_url + brands[i]
-		grailed_driver.get(grailed_url)
+		driver.get(grailed_url)
 
 		# filter items to be brand new
-		filters = grailed_driver.find_elements_by_class_name('instant-search-collapsible._collapsed')
+		filters = driver.find_elements_by_class_name('instant-search-collapsible._collapsed')
 		condition = filters[2]
 		condition.click()
-		new_box = grailed_driver.find_element_by_name('New/Never Worn')
+		# checks if Grailed is requesting authentication after clicking an element
+		authetication_xpath = "//div[@class=\"UsersAuthentication\"]"
+		requesting_authentication = check_element_exists(driver, authetication_xpath)
+		if (requesting_authentication): 
+			# clicks off the screen and tries to filter the items again
+			log_in_button = driver.find_element_by_link_text('Log in')
+			ActionChains(driver).move_to_element_with_offset(log_in_button, 500, 0).click().perform()
+			condition.click()
+		new_box = driver.find_element_by_name('New/Never Worn')
 		new_box.click()
 
 		# sort items by new
-		sort_select = grailed_driver.find_element_by_class_name('ais-SortBy-select')
+		sort_select = driver.find_element_by_class_name('ais-SortBy-select')
 		sort_select.click()
-		sorts = grailed_driver.find_elements_by_class_name('ais-SortBy-option')
+		sorts = driver.find_elements_by_class_name('ais-SortBy-option')
 		new_sort = sorts[4]
 		new_sort.click()
+		time.sleep(1.5)
 
 		# get all items up until max_age
 		xpath = "//div[@class=\"feed-item\"]//span[@class=\"date-ago\"]"
-		feed_ages = grailed_driver.find_elements_by_xpath(xpath)
+		feed_ages = driver.find_elements_by_xpath(xpath)
 		last_age = time_in_days(feed_ages[-1].text)
-		last_height = grailed_driver.execute_script("return document.body.scrollHeight")
+		last_height = driver.execute_script("return document.body.scrollHeight")
 		while last_age < max_age: 
-			grailed_driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+			driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 			time.sleep(1.5)
-			new_height = grailed_driver.execute_script("return document.body.scrollHeight")
-			feed_ages = grailed_driver.find_elements_by_xpath(xpath)
+			new_height = driver.execute_script("return document.body.scrollHeight")
+			feed_ages = driver.find_elements_by_xpath(xpath)
 			last_age = time_in_days(feed_ages[-1].text)
 			if new_height == last_height or last_age >= max_age: 
 				break
@@ -81,15 +93,15 @@ def main():
 
 		# get the item's brand, name, color, size, and price with shipping
 		xpath = "//div[@class=\"feed-item\"]/a[@class=\"listing-item-link\"]"
-		stale_links = grailed_driver.find_elements_by_xpath(xpath)
+		stale_links = driver.find_elements_by_xpath(xpath)
 		links = []
 		for stale_link in stale_links:
 			links.append(stale_link.get_attribute('href'))
 		items = []
 		for link in links: 
-			grailed_driver.get(link)
+			driver.get(link)
 			xpath = "//div[@class=\"-listing-designer-title-size\"]"
-			listing_info = grailed_driver.find_element_by_xpath(xpath).text.splitlines()
+			listing_info = driver.find_element_by_xpath(xpath).text.splitlines()
 			brand = listing_info[0]
 			name = listing_info[1]
 			size = parse_size(listing_info[2])
@@ -98,15 +110,23 @@ def main():
 				color = listing_info[3].lstrip('Color: ')
 				if "Multi" in color: 
 					color = ""
-			list_price = grailed_driver.find_element_by_class_name('-price').text.lstrip('$')
+			list_price = driver.find_element_by_class_name('-price').text.lstrip('$')
 			if ("," in list_price): 
 				list_price = list_price.replace(",", "")
-			shipping_price = grailed_driver.find_element_by_class_name('-shipping-cost').text.lstrip('+$')
+			shipping_price = driver.find_element_by_class_name('-shipping-cost').text.lstrip('+$')
 			if ("," in shipping_price): 
 				shipping_price = shipping_price.replace(",", "")
 			price = int(list_price) + int(shipping_price)
 			item = GrailedItem(link, brand, name, color, size, price)
 			items.append(item)
+	
+# check if element exists using xpath
+def check_element_exists(driver, xpath): 
+	try: 
+		driver.find_element_by_xpath(xpath)
+	except NoSuchElementException:
+		return False
+	return True
 
 # converts time into hours
 def time_in_days(age_text):
