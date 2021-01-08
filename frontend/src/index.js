e8b78d92bc9c3ea1981b9a918f6f307b9f16eca7
Kevin Diep <kevindiep@utexas.edu>
2021-01-08 16:36:48 -0600

    front end with navbar

---
+++ frontend/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './css/index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// Offline - serviceWorker.register(); 
+// Online - serviceWorker.unregister(); 
+serviceWorker.register();
